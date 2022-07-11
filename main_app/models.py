8593731bcf286ea75e9c8925b304583eec3fae4c
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-07-11 09:47:43 -0600

    Created my ride model and migrated to db

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import date
+from django.contrib.auth.models import User
+from django.urls import reverse
+from datetime import timedelta
 
 # Create your models here.
+class Ride(models.Model):
+    route = models.CharField(max_length=100)
+    date = models.DateField('ride date')
+    distance = models.FloatField()
+    elevation = models.IntegerField()
+    duration = models.DurationField(
+        default = timedelta(
+            hours=0,
+            minutes=0,
+            seconds=0,
+        )
+    )
+    avg_speed = models.FloatField(verbose_name=('Average Speed'))
+    description = models.TextField(max_length=300)
+    
+    
+    
